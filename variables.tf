e1b231ad1cabee26c2a813e9570d6e11b02ffbd8
insn0w <92552519+insn0w@users.noreply.github.com>
2022-10-30 10:38:03 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# ---autoscaling/variables.tf ---
+variable "public_cidrs" {
+  type = list(any)
+}
+
+variable "public_sn_count" {
+  type = number
+}
+
+variable "private_sn_count" {
+  type = number
+}
+
+variable "max_subnets" {
+  type = number
+}
